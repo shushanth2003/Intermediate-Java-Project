63b859c817f557743b77ff286f324bb8818b6b5a
shushanth2003 <bsshu2003@gmail.com>
2025-02-11 10:32:19 +0530

    Feat:"Add a File"

--- Library Management System/src/Main.java
+++ Library Management System/src/Main.java
@@ -1,17 +1,30 @@
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            FileInputStream fis = new FileInputStream("output.txt");
-            int i;
-            while ((i = fis.read()) != -1) {
-                System.out.print((char) i); // Convert byte to character and print
-            }
-            fis.close();
-        } catch (IOException e) {
-            System.out.println("Error reading file.");
-        }
+        boolean content=true;
+        Scanner in = new Scanner(System.in);
+       try{
+           while(content){
+               System.out.println("If you're not Member, Please click The Member Option please Enter as M");
+               char charMember = in.next().charAt(0);
+               if(charMember=='M'){
+                   Member member=new Member();
+               }
+               System.out.println("If You need to Add the BooK, Please add the Book, please Enter as A");
+               char charAddbook=in.next().charAt(0);
+               if(charAddbook=='A'){
+
+               }
+
+               System.out.println("If You need to Borrow the Book From Library, Please click the Borrow ");
+               String borrowBook=in.nextLine();
+               System.out.println("If You need to Return the Book From Library, Please click The Return Book");
+               String displayBook=in.nextLine();
+               System.out.println("If you need to Display The Book");
+           }
+       } catch (Exception e) {
+           System.err.println("Giving Input is error");
+       }
     }
 }
