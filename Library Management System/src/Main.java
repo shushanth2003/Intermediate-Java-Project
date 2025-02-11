67dd69d365bee3e2af3cca9259f056d4b79d45d7
shushanth2003 <bsshu2003@gmail.com>
2025-02-11 09:23:54 +0530

    Feat:"Add a File"

--- Library Management System/src/Main.java
+++ Library Management System/src/Main.java
@@ -1,5 +1,17 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            FileInputStream fis = new FileInputStream("output.txt");
+            int i;
+            while ((i = fis.read()) != -1) {
+                System.out.print((char) i); // Convert byte to character and print
+            }
+            fis.close();
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
     }
 }
